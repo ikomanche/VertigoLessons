9e9a6f6538532cba74e6e829136b104ec5d9d32d
ikomanche <kilincarslan98@gmail.com>
2021-02-03 01:14:24 +0300

    *refactor First Lesson

--- Assets/Scripts/FirstLessonScript.cs
+++ Assets/Scripts/FirstLessonScript.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 namespace LessonBasic
 {
     public class FirstLessonScript : MonoBehaviour
-    {
-        // Start is called before the first frame update
+    {   
         void Start()
         {
-
+            
         }
-
-        // Update is called once per frame
+        
         void Update()
         {
 
