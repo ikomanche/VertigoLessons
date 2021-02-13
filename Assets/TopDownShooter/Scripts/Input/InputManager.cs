35e3e49b7995223f9331e3e88e6788d745e259eb
ikomanche <kilincarslan98@gmail.com>
2021-02-13 17:04:48 +0300

    *commit ep3

--- Assets/TopDownShooter/Scripts/Input/InputManager.cs
+++ Assets/TopDownShooter/Scripts/Input/InputManager.cs
@@ -7,12 +7,16 @@ namespace TopDownShooter.PlayerInput
     public class InputManager : MonoBehaviour
     {
         [SerializeField] InputData _inputData;
-
+        [SerializeField] InputData _rotationInputData;
+        private Vector3 _lastMouseInput;
         private void Update()
         {
             _inputData.Horizontal = Input.GetAxis("Horizontal");
             _inputData.Vertical = Input.GetAxis("Vertical");
             //_inputData.Jump = Input.GetAxis("Jump");
+
+            _rotationInputData.Horizontal = Input.mousePosition.x - _lastMouseInput.x;
+            _lastMouseInput = Input.mousePosition;
         }
     }
 }
---
+++ Assets/TopDownShooter/Scripts/Movement/PlayerRotationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TopDownShooter.PlayerInput;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerMovement
+{
+    public class PlayerRotationController : MonoBehaviour
+    {
+        [SerializeField] InputData _inputData;
+        [SerializeField] Transform _tankTower;
+
+        private void Update()
+        {
+            _tankTower.Rotate(0, _inputData.Horizontal, 0, Space.Self);
+        }
+    }
+}
