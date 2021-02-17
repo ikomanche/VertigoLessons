78b5261fd3fd09adb57c8b354fc62ec170c5e567
ikomanche <kilincarslan98@gmail.com>
2021-02-17 16:48:45 +0300

    *refoctor input system

--- Assets/TopDownShooter/Scripts/Input/InputData.cs
+++ Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -6,9 +6,64 @@ namespace TopDownShooter.PlayerInput
 {
     [CreateAssetMenu(menuName = "TopDown Shooter/User Input/Input Data")]
     public class InputData : ScriptableObject
-    {
+    {        
         public float Horizontal;
         public float Vertical;
+
+        [Header("Axis Base Control")]
+        [SerializeField] private bool _axisActive;
+        [SerializeField] private string AxisNameHorizontal;
+        [SerializeField] private string AxisNameVertical;
+
+        [Header("Key Base Control")]
+        [SerializeField] private bool _keyBaseHorizontalActive;
+        [SerializeField] private KeyCode PositiveHorKeyCode;
+        [SerializeField] private KeyCode NegativeHorKeyCode;
+        [SerializeField] private bool _keyBaseVerticalActive;
+        [SerializeField] private KeyCode PositiveVerKeyCode;
+        [SerializeField] private KeyCode NegativeVerKeyCode;
+        [SerializeField] private float _increaseAmount = 0.015f;
         //public float Jump;
+
+        public void ProcessInput()
+        {
+            if(_axisActive)
+            {
+                Horizontal = Input.GetAxis(AxisNameHorizontal);
+                Vertical = Input.GetAxis(AxisNameVertical);
+            }
+            else
+            {
+                if(_keyBaseHorizontalActive)
+                {
+                    KeyBaseAxisControl(ref Horizontal, PositiveHorKeyCode, NegativeHorKeyCode);
+                }
+                if(_keyBaseVerticalActive)
+                {
+                    KeyBaseAxisControl(ref Vertical, PositiveVerKeyCode, NegativeVerKeyCode);
+                }
+            }            
+        }
+
+        private void KeyBaseAxisControl(ref float value, KeyCode positive, KeyCode negative)
+        {
+            bool positiveActive = Input.GetKey(positive);
+            bool negativeActive = Input.GetKey(negative);            
+
+            if (positiveActive)
+            {
+                value += _increaseAmount;
+            }
+            else if (negativeActive)
+            {
+                value -= _increaseAmount;
+            }
+            else
+            {
+                value = 0;
+            }
+
+            value = Mathf.Clamp(value, -1, 1);
+        }
     }
 }
--- Assets/TopDownShooter/Scripts/Input/InputManager.cs
+++ Assets/TopDownShooter/Scripts/Input/InputManager.cs
@@ -6,17 +6,23 @@ namespace TopDownShooter.PlayerInput
 {
     public class InputManager : MonoBehaviour
     {
-        [SerializeField] InputData _inputData;
-        [SerializeField] InputData _rotationInputData;
-        private Vector3 _lastMouseInput;
+        //[SerializeField] InputData _movementInput;
+        //[SerializeField] InputData _rotationInput;
+        [SerializeField] private InputData[] _inputDataArray;
+        //private Vector3 _lastMouseInput;
         private void Update()
         {
-            _inputData.Horizontal = Input.GetAxis("Horizontal");
-            _inputData.Vertical = Input.GetAxis("Vertical");
-            //_inputData.Jump = Input.GetAxis("Jump");
+            for (int i = 0; i < _inputDataArray.Length; i++)
+            {
+                _inputDataArray[i].ProcessInput();
+            }
 
-            _rotationInputData.Horizontal = Input.mousePosition.x - _lastMouseInput.x;
-            _lastMouseInput = Input.mousePosition;
+            //_movementInput.Horizontal = Input.GetAxis("Horizontal");
+            //_movementInput.Vertical = Input.GetAxis("Vertical");
+            //_inputData.Jump = Input.GetAxis("Jump"); //Jump Homework
+
+            //_rotationInputData.Horizontal = Input.mousePosition.x - _lastMouseInput.x;
+            //_lastMouseInput = Input.mousePosition;
         }
     }
 }
