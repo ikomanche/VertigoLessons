d4a7c2dee62b8babbc60e27c5447a3d7d6ba51c1
ikomanche <kilincarslan98@gmail.com>
2021-02-19 20:05:59 +0300

    *scriptable object initializer

---
+++ Assets/TopDownShooter/Scripts/Inventory/ScriptableShootManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.Inventory
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Inventory/Scriptable Shoot Manager")]
+    public class ScriptableShootManager : AbstractScriptableManager<ScriptableShootManager>
+    {
+        public void Shoot()
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            Debug.Log("ShootManager Initialize");
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+            Debug.Log("ShootManager Destroy");
+        }
+    }
+}
